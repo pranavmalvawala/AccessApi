0b3fbff1c9aba528ef604bd41dcfa4de95b3d708
pranavmalvawala <pranavmalvawala@gmail.com>
2021-01-30 08:47:06 +0530

    Update permission list

--- src/helpers/Permissions.ts
+++ src/helpers/Permissions.ts
@@ -1,21 +1,21 @@
 import { Permissions as BasePermissions } from '../apiBase/helpers'
 
 export class Permissions extends BasePermissions{
-    static server: {
+    static server = {
         admin: { contentType: "Server", action: "Admin" }
     }
 
-    static roles: {
+    static roles = {
         edit: { contentType: "Roles", action: "Edit" },
         view: { contentType: "Roles", action: "View" }
     }
 
-    static roleMembers: {
+    static roleMembers = {
         view: { contentType: "RoleMembers", action: "View" },
         edit: { contentType: "RoleMembers", action: "Edit" }
     }
 
-    static rolePermissions: {
+    static rolePermissions = {
         view: { contentType: "RolePermissions", action: "View" },
         edit: { contentType: "RolePermissions", action: "Edit" }
     }
