2cd26325d27892654f12e7f8c7d73b248b5a5f4c
Jeremy Zongker <jeremy@trilitech.com>
2021-03-22 17:59:35 +0000

    Removed temp logging

--- lambda.js
+++ lambda.js
@@ -1,14 +1,11 @@
 const { createServer, proxy } = require('aws-serverless-express');
 const { init } = require('./dist/App');
 const { Pool } = require('./dist/apiBase/pool');
-const { LoggingHelper } = require('./dist/apiBase/helpers/LoggingHelper');
-
 
 Pool.initPool();
 
 module.exports.universal = function universal(event, context) {
     init().then(app => {
-        LoggingHelper.getCurrent().info(JSON.stringify(event));
         const server = createServer(app);
         return proxy(server, event, context);
     });
