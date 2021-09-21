466f00289e9664979c37041a0f73a3ffae3be6ef
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-09-21 09:32:59 +0530

    remove applicationApis table (#88)

--- src/repositories/PermissionRepository.ts
+++ src/repositories/PermissionRepository.ts
@@ -2,11 +2,6 @@ import { DB } from "../apiBase/db";
 import { Permission } from "../models";
 
 export class PermissionRepository {
-    public loadForApp(appName: string): Promise<Permission[]> {
-        return DB.query("SELECT * FROM permissions WHERE apiName IN (SELECT apiName from applicationApis WHERE applicationName=?) ORDER BY apiName, displaySection, displayAction", [appName])
-            .then((rows: Permission[]) => { return rows; });
-    }
-
     public loadAll(): Promise<Permission[]> {
         return DB.query("SELECT * FROM permissions ORDER BY apiName, displaySection, displayAction", [])
             .then((rows: Permission[]) => { return rows; });
--- tools/initdb.ts
+++ tools/initdb.ts
@@ -9,7 +9,6 @@ const init = async () => {
 
   const tables: { title: string, file: string }[] = [
     { title: "APIs", file: "apis.mysql" },
-    { title: "Application APIs", file: "applicationApis.mysql" },
     { title: "Churches", file: "churches.mysql" },
     { title: "Permissions", file: "permissions.mysql" },
     { title: "Role Members", file: "roleMembers.mysql" },
